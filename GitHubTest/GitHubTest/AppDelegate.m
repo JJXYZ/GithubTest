d03d7b3372a582747d225b84513c0a35263f3604
Jay <hzhjjie@163.com>
2016-09-29 15:38:46 +0800

    Add 1

---
+++ GitHubTest/GitHubTest/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  GitHubTest
+//
+//  Created by Jay on 2016/9/29.
+//  Copyright © 2016年 JayJJ. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+@end
---
+++ GitHubTest/GitHubTest/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  GitHubTest
+//
+//  Created by Jay on 2016/9/29.
+//  Copyright © 2016年 JayJJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"Add 1");
+}
+
+
+@end
